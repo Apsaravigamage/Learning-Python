6677ae1136a9cbb309c56ca8abad496899c2b6c6
Apsaravigamage <49783705+Apsaravigamage@users.noreply.github.com>
2020-05-02 16:01:35 +0530

    Add files via upload
    BitWise Operators & import math function

---
+++ bitwis & math function.py
@@ -0,0 +1,42 @@
+# bitwise Operators (complement,And,Or,Xor,lefrshift,Rightshift)
+print(~12)
+print(~16)
+
+print(12&13)
+print(12|13)
+print(12^13)
+print(10<<2)
+print(10>>2)
+
+# math function
+import math
+
+x=math.sqrt(25)
+print(x)
+x=math.sqrt(15)
+print(x)
+print(math.floor(2.9)) #last least value
+print(math.ceil(2.9))
+print(math.pow(3,2))# 3**2
+print(math.pi)
+print(math.e)
+
+import math as m
+print(math.sqrt(25))
+print(m.sqrt(25))
+
+#don't wont to import all the function
+from math import sqrt,pow
+print(pow (4,5)) # don't specify math
+print(help(math)) # have function
+
+
+
+
+
+
+
+
+
+
+
