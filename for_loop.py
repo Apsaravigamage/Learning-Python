d5c8d646620a3364a6c0c0294d8672155e8f6dae
Apsaravigamage <49783705+Apsaravigamage@users.noreply.github.com>
2020-05-03 20:57:57 +0530

    Add files via upload
    For loop

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+# work with sequence
+x=['cat',4.76,34,'dog']
+for i in x: # print one by one "i"is one element
+    print(i)
+
+x='SRI LANKA' #String
+for i in x:
+    print(i)
+
+for i in range(10): # 1 to 10
+    print(i)
+
+for i in range(11,21,1):# 11 to 20 1 by 1 itretion
+    print(i)
+
+for i in range(20,10,-1):# 20 to 10
+    print(i)
+
+
+for i in range(1,21): # divisible by 5
+    if(i%5!=0):
+        print(i)
+
+
+for i in range(1,10):
+    print(i*i)
+
