3e7fdfe8df0ca16a0bc240a074e10eb4fd69867c
Apsaravigamage <49783705+Apsaravigamage@users.noreply.github.com>
2020-05-05 17:56:40 +0530

    Add files via upload
    Patterns

---
+++ patterns.py
@@ -0,0 +1,26 @@
+i=1
+while i<=4:
+    print("# ",end="")
+    j=1
+    while j<=3:
+        print("# ",end="")
+        j=j+1
+    i=i+1
+    print()
+
+for i in range(4):
+    for j in range (4):
+        print("# ",end="")
+    print()
+
+
+for i in range(4):
+    for j in range (i+1):
+        print("# ",end="")
+    print()
+
+
+for i in range(4):
+    for j in range (4-i):
+        print("# ",end="")
+    print()
